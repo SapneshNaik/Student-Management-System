266184de6abeafa72ad0e57421d0b6ab24e2db82
Sapnesh Naik <sapnesh.naik@intel.com>
2020-03-06 20:45:24 +0530

    fix

--- src/backend/routes/api.php
+++ src/backend/routes/api.php
@@ -1,6 +1,12 @@
 <?php
 
+
 use App\Constants;
+
+
+header('Access-Control-Allow-Origin:  *');
+header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
 
 Route::group([
     'prefix' => 'v1',
